6cde76c28899680c458624183dfe5d52d228f12b
alireza375 <alirezaraju8@gmail.com>
2026-09-03 23:46:16 +0600

    service update

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Services\Admin\BlogService;
+use App\Http\Services\BlogService;
 use Illuminate\Http\Request;
 
 class BlogController extends Controller
--- app/Http/Services/BlogService.php
+++ app/Http/Services/BlogService.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Services\Admin;
+namespace App\Http\Services;
 
 use App\Models\Blog;
 use Illuminate\Http\Request;
